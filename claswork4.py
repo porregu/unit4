112e48eb230e33ecfc849e753f7efcad549ee7c1
guillermo <porregu@ssfs.org>
2019-10-08 14:09:36 -0400

    do flowchart and start unit 4 project

---
+++ claswork4.py
@@ -0,0 +1,15 @@
+import random
+
+X = (float(input("what you want to be the maximun ")))
+def random():
+    a =random.randint(0,X)
+    b =random.randint(0,X)
+
+
+    choice = (input("what problem you want to use + - *"))
+
+    if  (choice=="x",(a*b)):
+    if (choice=="-",(a-b)):
+    else: (choice==(a+b))
+
+
