7493d4d1a1cd3d9840f8f0eeb70d500ab979cae5
guillermo <porregu@ssfs.org>
2019-10-09 14:35:40 -0400

    work in unit 4 project

--- claswork4.py
+++ claswork4.py
@@ -1,15 +1,24 @@
 import random
 
 X = (float(input("what you want to be the maximun ")))
-def random():
+def problem():
     a =random.randint(0,X)
     b =random.randint(0,X)
+    D=(input("what time of problem you want addition (+) subtraction (-) or multiplication (*)"))
+    if D != "*" and D != "-":
+        D="+"
+    answer=int (input ("what is"+str(a)+D+str(b)))
+    if D =="*" >a*b==answer:
+        print("right")
+        if D =="-" > a+b==answer:
+            if  a-b==answer:
+                print("right")
+    else:
+        print("wrong")
 
 
-    choice = (input("what problem you want to use + - *"))
 
-    if  (choice=="x",(a*b)):
-    if (choice=="-",(a-b)):
-    else: (choice==(a+b))
+problem()
+
 
 
